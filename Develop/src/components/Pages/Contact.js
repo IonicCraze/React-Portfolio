c36d7b587cef560733a337f6353319aef50e078d
IonicCraze <daviddeitz97@gmail.com>
2022-07-18 16:33:44 -0400

    changed folder to deploy

---
+++ Develop/src/components/Pages/Contact.js
@@ -0,0 +1,22 @@
+
+
+
+function Contact(){ 
+return (
+<main>
+    <h2>Contact Me</h2>
+    <form className="form-group" id="contact-form">
+      <ul>
+        <li>Email: daviddeitz97@gmail.com</li>
+        <li>Phone: 740-652-5791</li>
+        <li>Github: https://github.com/IonicCraze</li>
+      </ul>
+    </form>
+</main>
+);
+
+}
+
+
+
+export default Contact;
---
+++ Develop/src/components/Pages/Resume.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Resume() {
+	return (
+		<main>
+			<h3 id="resume">Resume</h3>
+
+			<div className="proficiencies">
+				<h3>The Ohio State University Boot Camps, Certificate in Coding.</h3>
+				<p>An intensive 12-week long boot camp dedicated to designing and building web applications</p>
+				<p>Web Development Proficiencies:</p>
+				<ul>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>JavaScript</li>
+					<li>React.js</li>
+				    <li>APIs</li>
+					<li>Node.js</li>
+					<li>Express.js</li>
+					<li>MYSQL</li>
+                    <li>Sequelize</li>
+					<li>MongoDB</li>
+                    <li>Mongoose</li>
+				</ul>
+				<br></br>
+			
+			</div>
+		</main>
+	);
+}
+
+export default Resume;
